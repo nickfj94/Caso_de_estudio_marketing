bae4a2bce15152eea3d8735cae3fa21f97d737bd
cgp2409 <65213431+cgp2409@users.noreply.github.com>
2022-07-27 11:57:37 -0500

    Add files via upload

--- caso_de_estudio.py
+++ caso_de_estudio.py
@@ -16,6 +16,11 @@ import numpy as np
 import sqlite3 as sql
 from matplotlib.pyplot import figure
 import seaborn as sns
+from sklearn.metrics import mean_squared_error
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import NearestNeighbors
+import matplotlib.pyplot as plt
+import sklearn
 
 """
 Solución
@@ -142,5 +147,52 @@ df.groupby(['year_rating'])['rating'].count().sort_values(ascending=False)
 # Cantidad de calificaciones por año de la pelicula
 df.groupby(['year'])['rating'].count().sort_values(ascending=False)
 
-### ALGORITMO
+### ------------------ALGORITMOS----------------
 
+#---------filtro colaborativo
+#---------basado en usuario
+#---------metodo: distancia por coseno de los vectores
+
+
+#matriz en la que cruzamos todos los usuarios con todos las peliculas
+matriz = pd.pivot_table(df_users, values='rating', index='userId', columns='movieId').fillna(0)
+
+#porcentaje de sparcity
+ratings = matriz.values
+sparsity = float(len(ratings.nonzero()[0]))
+sparsity /= (ratings.shape[0] * ratings.shape[1])
+sparsity *= 100
+print('Sparsity: {:4.2f}%'.format(sparsity))
+
+#conjunto de entrenamiento y de prueba
+ratings_train, ratings_test = train_test_split(ratings, test_size = 0.33, random_state=42)
+print(ratings_train.shape)
+print(ratings_test.shape)
+
+#matriz la similitud entre usuarios
+sim_matrix = 1 - sklearn.metrics.pairwise.cosine_distances(ratings)
+print(sim_matrix.shape)
+
+#ver similud graficamente
+plt.imshow(sim_matrix);
+plt.colorbar()
+plt.show()
+
+#separar las filas y columnas de train y test
+sim_matrix_train = sim_matrix[0:408,0:408]
+sim_matrix_test = sim_matrix[408:610,408:610]
+users_predictions = sim_matrix_train.dot(ratings_train) / np.array([np.abs(sim_matrix_train).sum(axis=1)]).T
+
+#ver recomendaciones entre todas las peliculas y usuarios graficamente
+plt.rcParams['figure.figsize'] = (20.0, 5.0)
+plt.imshow(users_predictions);
+plt.colorbar()
+plt.show()
+
+#ejemplo de prediccón de usuario
+usuario_ver = 0 #data.iloc[0]['userId'] - 1 # resta 1 para obtener el index de pandas.
+user0=users_predictions.argsort()[usuario_ver]
+# ver los 10 recomendados con mayor puntaje en la predic para este usuario
+for i, aRepo in enumerate(user0[-10:]):
+    selRepo = df_movies[df_movies['movieId']==(aRepo+1)]
+    print(selRepo['title'] ,selRepo['movieId'], 'puntaje:', users_predictions[usuario_ver][aRepo])
